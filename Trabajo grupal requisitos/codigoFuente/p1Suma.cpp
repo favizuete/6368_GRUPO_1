99104f6f831ead000ea06411989049cc438c89a0
favi10 <67701116+favi10@users.noreply.github.com>
2020-07-06 15:58:26 -0500

    Add files via upload

---
+++ Trabajo grupal requisitos/codigoFuente/p1Suma.cpp
@@ -0,0 +1,16 @@
+
+
+#include <conio.h>
+#include <stdio.h>
+
+int main()
+{
+    float n1, n2,suma;
+
+    printf( "\nIntroduzca primer numero: " );
+    scanf( "%f", &n1 );
+    printf( "\nIntroduzca segundo numero: " );
+    scanf( "%f", &n2 );
+    suma = n1 + n2;
+    printf( "\nLa suma es: %2.2f", suma );
+    }
---
+++ Trabajo grupal requisitos/codigoFuente/p5Multipli.cpp
@@ -0,0 +1,27 @@
+// multipliacionNumeros.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    float num1, num2, multiplicacion;
+    std::cout << "\tMultiplicacion de dos numeros!\n";
+
+    printf("Ingrese un numero: ");
+    scanf_s("%f", &num1);
+    printf("\nIngrese un numero: ");
+    scanf_s("%f", &num2);
+    multiplicacion = num1 * num2;
+    printf("\nEl resultado de la multiplicacion es: %2.2f", multiplicacion);
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
